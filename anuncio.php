284e67e35487ca7436608034c102c3e5be6a7891
ssalgadoi <se.salgado@duocuc.cl>
2024-08-26 07:06:45 -0400

    Creo propiedades de la pagina principal y anuncios dinamicos

--- anuncio.php
+++ anuncio.php
@@ -1,42 +1,63 @@
 <?php
     require 'includes/funciones.php';
-   
     incluirTemplate('header');
+
+    // Obtener el ID de la propiedad desde la URL
+    $id = $_GET['id'] ?? null;
+
+    // Verificar si hay un ID válido
+    if (!$id) {
+        header('Location: /');
+        exit;
+    }
+
+    // Importar la conexión a la base de datos
+    require 'includes/config/database.php';
+    $db = conectarBD();
+
+    // Sanitizar el ID
+    $id = mysqli_real_escape_string($db, $id);
+
+    // Consulta para obtener los detalles de la propiedad
+    $query = "SELECT * FROM propiedades WHERE id = {$id}";
+    $resultado = mysqli_query($db, $query);
+
+   
+
+    $propiedad = mysqli_fetch_assoc($resultado);
 ?>
 
+<main class="contenedor seccion contenido-centrado">
+    <h1><?php echo $propiedad['titulo']; ?></h1>
 
-    <main class="contenedor seccion contenido-centrado">
-        <h1>Casa en Venta frente al bosque</h1>
+    <img loading="lazy" src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt="imagen de la propiedad">
 
-        <picture>
-            <source srcset="build/img/destacada.webp" type="image/webp">
-            <source srcset="build/img/destacada.jpg" type="image/jpeg">
-            <img loading="lazy" src="build/img/destacada.jpg" alt="imagen de la propiedad">
-        </picture>
+    <div class="resumen-propiedad">
+        <p class="precio">$<?php echo $propiedad['precio']; ?></p>
+        <ul class="iconos-caracteristicas">
+            <li>
+                <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
+                <p><?php echo $propiedad['wc']; ?></p>
+            </li>
+            <li>
+                <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
+                <p><?php echo $propiedad['estacionamiento']; ?></p>
+            </li>
+            <li>
+                <img class="icono"  loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
+                <p><?php echo $propiedad['habitaciones']; ?></p>
+            </li>
+        </ul>
 
-        <div class="resumen-propiedad">
-            <p class="precio">$3,000,000</p>
-            <ul class="iconos-caracteristicas">
-                <li>
-                    <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
-                    <p>3</p>
-                </li>
-                <li>
-                    <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
-                    <p>3</p>
-                </li>
-                <li>
-                    <img class="icono"  loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
-                    <p>4</p>
-                </li>
-            </ul>
+        <p><?php echo $propiedad['descripcion']; ?></p>
+    </div>
+</main>
 
-            <p>Proin consequat viverra sapien, malesuada tempor tortor feugiat vitae. In dictum felis et nunc aliquet molestie. Proin tristique commodo felis, sed auctor elit auctor pulvinar. Nunc porta, nibh quis convallis sollicitudin, arcu nisl semper mi, vitae sagittis lorem dolor non risus. Vivamus accumsan maximus est, eu mollis mi. Proin id nisl vel odio semper hendrerit. Nunc porta in justo finibus tempor. Suspendisse lobortis dolor quis elit suscipit molestie. Sed condimentum, erat at tempor finibus, urna nisi fermentum est, a dignissim nisi libero vel est. Donec et imperdiet augue. Curabitur malesuada sodales congue. Suspendisse potenti. Ut sit amet convallis nisi.</p>
+<?php
+    incluirTemplate('footer');
+?>
 
-            <p>Aliquam lectus magna, luctus vel gravida nec, iaculis ut augue. Praesent ac enim lorem. Quisque ac dignissim sem, non condimentum orci. Morbi a iaculis neque, ac euismod felis. Fusce augue quam, fermentum sed turpis nec, hendrerit dapibus ante. Cras mattis laoreet nibh, quis tincidunt odio fermentum vel. Nulla facilisi.</p>
-        </div>
-    </main>
+<?php 
+    mysqli_close($db);
 
-    <?php
-     incluirTemplate('footer');
-?> 
+?>
