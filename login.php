d955eebfdd7739079b225067d6343541996a74af
ssalgadoi <se.salgado@duocuc.cl>
2024-08-26 10:06:32 -0400

    Validacion del Formularion

--- login.php
+++ login.php
@@ -1,4 +1,32 @@
 <?php
+
+    require  'includes/config/database.php';
+
+    $db = conectarBD();
+
+    $errores = [];
+
+    // autenticar usuario
+    if ( $_SERVER['REQUEST_METHOD'] === 'POST') {
+        var_dump($_POST);
+
+        $email = mysqli_real_escape_string($db, filter_var($_POST['email'], FILTER_VALIDATE_EMAIL ));
+        // var_dump($email);
+        $password = mysqli_real_escape_string( $db, $_POST['password']);
+
+
+        if ( !$email ) {
+            $errores[] = "El email es obligatorio o no es valido";
+        }
+        if ( !$password ) {
+            $errores[] = "El Pass word es obligatorio";
+        }
+
+
+    }
+
+
+
     require 'includes/funciones.php';
    
     incluirTemplate('header');
@@ -6,16 +34,21 @@
 
     <main class="contenedor seccion contenido-centrado">
         <h1>Iniciar seccion</h1>
+        <?php foreach($errores as $error) :?>
+            <div class="alerta error">
+                <?php echo $error;?>
+            </div>
+        <?php endforeach; ?>
 
-        <form action="" class="formulario">
+        <form action="" class="formulario" method="POST" novalidate>
         <fieldset>
                 <legend>Datos del Usuario</legend>
 
                 <label for="email">E-mail</label>
-                <input type="email" placeholder="Tu Email" id="email">
+                <input type="email" name="email" placeholder="Tu Email" id="email" required>
 
                 <label for="password">Password</label>
-                <input type="password" placeholder="Tu Password" id="password">
+                <input type="password" name="password" placeholder="Tu Password" required id="password">
 
                 <!-- <label for="mensaje">Mensaje:</label>
                 <textarea id="mensaje"></textarea> -->
